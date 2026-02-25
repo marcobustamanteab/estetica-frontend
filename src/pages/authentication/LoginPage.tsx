151a26374bc6f5ab934453cf32c788036b84bd31
Marco Bustamante <marco.bustamante@zentagroup.com>
2026-02-25 15:30:57 -0300

    unnuse variable

--- src/pages/authentication/LoginPage.tsx
+++ src/pages/authentication/LoginPage.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import Spinner from '../../components/common/Spinner';
 import './loginPage.css';
